4c8951c90263acc567b89743c10f0bbe38092154
Jorge Valencia <jorjazo@gmail.com>
2020-05-16 17:52:30 -0400

    Commit inicial

---
+++ src/test/java/dev/jorjazo/homeapi/HomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jorjazo.homeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
